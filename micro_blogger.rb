469f84442b6c00f5d60b175d242e73a22943a50f
Tomasz Kula <tomaszkula07@gmail.com>
2016-01-21 20:48:01 +0100

    Can send tweets, can dm msg my followers

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -9,10 +9,47 @@ class MicroBlogger
   end
 
   def tweet(message)
-    @client.update(message)
+    @client.update(message) if message.size <= 140
   end
+
+  def dm(target, message)
+    screen_names = @client.followers.collect { |follower| @client.user(follower).screen_name }
+
+    puts "Trying to send #{target} this direct message:"
+    puts message
+    message = "d @#{target} #{message}"
+
+    if screen_names.include? target
+      tweet(message)
+    else
+      puts "Can only DM people who follow you!"
+    end
+  end
+
+
+  def run
+    puts "Welcome to the JSL Twitter Client!"
+    command = ""
+    while command != "q"
+      printf "Enter command: "
+      input = gets.chomp
+      parts = input.split(" ")
+      command = parts[0]
+      case command
+        when 'q' then puts "Goodbye!"
+        when 't' then tweet(parts[1..-1].join(" "))
+        when 'dm' then dm(parts[1], parts[2..-1].join(" "))
+
+        else
+          puts "Sorry, I don't know how to #{command}"
+      end
+    end
+  end
+
 
 end
 
 blogger = MicroBlogger.new
-blogger.tweet("MicroBlogger Initialized")
+
+blogger.run
+
